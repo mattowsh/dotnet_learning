e0c89f6570d541b6ede4c17d94c980d8d8585f84
mattowsh <mattobelen@gmail.com>
2023-03-28 13:49:16 -0300

    first records was added and available in Especialidad endpoint

--- first_project/Controllers/EspecialidadController.cs
+++ first_project/Controllers/EspecialidadController.cs
@@ -1,19 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using first_project.Models;
+using System.Linq;
 
 namespace first_project.Controllers
 {
     public class EspecialidadController : Controller
     {
+
+        // We need an object to intializate in our constructor. Thus, we will
+        // create this object called _context, of type first_projectContext:
+        private readonly first_projectContext _context;
+
         // Constructor:
-        public EspecialidadController()
+        // Constructor receive an object called "context", of type first_projectContext
+        public EspecialidadController(first_projectContext context)
         {
-            
+            _context = context;
         }
 
         // Actions:
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Especialidad.ToList());
         }
     }
 }
